2ed50a306c5ceb2c55379c3660f3add0c207e732
Niraj-Vasani <himanshujogani72020@gmail.com>
2022-07-08 17:52:00 +0530

    Intro Screen UI

--- make_my_trip/lib/features/intro/presentation/widgets/indicator_widget.dart
+++ make_my_trip/lib/features/intro/presentation/widgets/indicator_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:make_my_trip/core/theme/make_my_trip_colors.dart';
 import 'package:make_my_trip/features/intro/presentation/cubit/intro_cubit.dart';
 import 'package:make_my_trip/features/intro/presentation/cubit/intro_state.dart';
 
@@ -10,22 +11,42 @@ class Indicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.only(left: 20, right: 20),
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
       child: BlocBuilder<IntroCubit, IntroState>(
         builder: (context, state) {
           return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
+              AbsorbPointer(
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                      (state is IndicatorIndexState && state.index == 3)
+                          ? ""
+                          : "Skip",
+                      style: const TextStyle(
+                          fontSize: 16, color: MakeMyTripColors.accentColor)),
+                ),
+              ),
               DotsIndicator(
                 dotsCount: 4,
                 position: (state is IndicatorIndexState) ? state.index : 0,
                 decorator: DotsDecorator(
                   activeSize: const Size(24.0, 9.0),
-                  activeColor: Colors.blue,
-                  color: Colors.black,
+                  activeColor: MakeMyTripColors.accentColor,
+                  color: MakeMyTripColors.colorBlack,
                   activeShape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5.0)),
                 ),
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                    (state is IndicatorIndexState && state.index == 3)
+                        ? "Done"
+                        : "Next",
+                    style: const TextStyle(
+                        fontSize: 16, color: MakeMyTripColors.accentColor)),
               ),
             ],
           );
