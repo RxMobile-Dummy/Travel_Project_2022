ff1e35e3682ab2fc0ee2f74ec03420d4b7aaf024
Padmanava <karpadmanava16@gmail.com>
2022-07-08 19:17:36 +0530

    feat(sign_up_page):  added signup page design

--- make_my_trip/lib/core/navigation/route_info.dart
+++ make_my_trip/lib/core/navigation/route_info.dart
@@ -1,5 +1,5 @@
 /// this class will contain static route names
 class RoutesName {
-  static const splash = '/signup';
-  static const signup = '/';
+  static const splash = '/';
+  static const signup = '/signup';
 }
