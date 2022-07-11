653ec59b0f8573a7b8cae75b80dcfb3d463a9fc4
Nidhi Gokani <nidhi.gokani@pc0383.local>
2022-07-11 13:12:19 +0530

    feat(homepage_tour_api): get_tour_deatils

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -3,12 +3,18 @@ const app: Express = express();
 import mongoose from 'mongoose';
 const connection = mongoose.connect('mongodb://localhost:27017/mmtdata');
 import { router as hotelroute} from  './controller/hotel_controller';
+import { router as tourroute} from  './controller/tour_controller';
 import { router as cityroute } from './controller/city_controller';
 import * as dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT;
 app.use(express.json());
 
+
+
+
+//tour route
+app.use('/tour',tourroute)
 
 app.use('/hotel',hotelroute);
 app.use('/city',cityroute);
