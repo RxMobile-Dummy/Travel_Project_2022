156ab773612639c5502bc39f3b5ff9129c4d2a83
Nikhil <nikkhorwal@gmail.com>
2022-07-11 19:18:38 +0530

    intro screen complete

--- make_my_trip/lib/main.dart
+++ make_my_trip/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:make_my_trip/core/navigation/route_info.dart';
 import 'package:make_my_trip/core/theme/make_my_trip_theme.dart';
+import 'features/hotel_listing/hotel_listing_injection_container.dart' as di;
+
 import './core/navigation/app_router.dart' as AppRoutes;
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
