92452da1af95de92043117809e691ca466a83ee4
Akash2811000 <akashgupta7383@gmail.com>
2022-07-12 12:55:37 +0530

    update variable name

--- Backend/src/controller/hotel_controller.ts
+++ Backend/src/controller/hotel_controller.ts
@@ -4,16 +4,26 @@ var router = express.Router();
 
 class HotelController {
     static async getHotel(req:Request , res : Response){
-        const hoteldomain = new HotelDomain();
-        await hoteldomain.getAllHotel(req,res);
+        const hotelDomain = new HotelDomain(); 
+       await hotelDomain.getAllHotel(req,res);
+
     }
     // get hotel by search
     static async getHotelBySearch(req:Request , res : Response){
-        const hoteldomain = new HotelDomain();
-       await hoteldomain.getHotelBySearch(req,res);
+        const hotelDomain = new HotelDomain();
+       await hotelDomain.getHotelBySearch(req,res);
+
+    }
+
+    // get hot by city and room
+    static async getHotelBycityroom(req:Request , res : Response){
+        const hotelDomain = new HotelDomain();
+       await hotelDomain.getHotelByCityRoom(req,res);
+
     }
 }
 
 router.get('/',HotelController.getHotel);
 router.get('/:hotelsearch',HotelController.getHotelBySearch);
+router.get('/:cityname/:roomcount',HotelController.getHotelBycityroom);
 export{router};
