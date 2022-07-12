c88bec5bf34923b3eaf4b354f5857271c64c6a31
Akash2811000 <akashgupta7383@gmail.com>
2022-07-12 09:57:54 +0530

    add status code file

--- Backend/src/domain/city_domain.ts
+++ Backend/src/domain/city_domain.ts
@@ -1,27 +1,28 @@
 import { citymodel } from "../model/city";
+import { StatusCode } from "../statuscode";
 import express, { Express, Request, Response } from 'express'
 
-class CityDomain{
-    async getAllCity(req: Request , res: Response){
-        try{
-            citymodel.find({}, function (error:Error, result:String) {
-                if(result){
-                res.send(result);
-               
-                res.end();
-                }else {
-                    res.send(error);
+class CityDomain {
+    async getAllCity(req: Request, res: Response) {
+        try {
+            citymodel.find({}, function (error: Error, result: String) {
+                if (result) {
+                    res.status(StatusCode.Sucess).send(result);
+
+                    res.end();
+                } else {
+                    res.status(StatusCode.Server_Error).send(error);
                     res.end();
                 }
                 res.end();
-              }).select('_id city_name')
+            }).select('_id city_name')
         }
         catch (err: any) {
-            res.status(500).send(err.message);
+            res.status(StatusCode.Server_Error).send(err.message);
             res.end();
 
         }
     }
 }
 
-export {CityDomain};
+export { CityDomain };
