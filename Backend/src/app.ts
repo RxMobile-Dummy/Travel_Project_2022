ea0fd23f1b8fc831340d86c24e34e52e7a3dcc5a
Nidhi Gokani <nidhi.gokani@pc0383.local>
2022-07-13 16:15:28 +0530

    feat(authentication_backend) : introduce authentication

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,13 +5,22 @@ const connection = mongoose.connect('mongodb://localhost:27017/mmtdata');
 import { router as hotelroute} from  './controller/hotel_controller';
 import { router as tourroute} from  './controller/tour_controller';
 import { router as cityroute } from './controller/city_controller';
+import { verifyToken,checkRequest } from "./authentication/verify_token";
+import * as admin from 'firebase-admin';
+import credential from "./travelproject22-6b9d4-firebase-adminsdk-2wiay-c9c1876710.json";
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT;
 app.use(express.json());
-
-
+//firebase intitialize 
+admin.initializeApp(
+    {
+        credential: admin.credential.cert(JSON.parse(JSON.stringify(credential)))
+    }
+);
+//Token verification call
+app.use(verifyToken,checkRequest);
 
 app.use('/hotel',hotelroute)
 app.use('/tour',tourroute)
@@ -19,7 +28,7 @@ app.use('/tour',tourroute)
 app.use('/hotel',hotelroute);
 app.use('/city',cityroute);
 
-app.get('/', (req: Request, res: Response) => {
+app.post('/', (req: Request, res: Response) => {
     res.send('MMT Backend development');
     res.end();
 });
