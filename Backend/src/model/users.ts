e1cf8533a50f04e854c489fadd9193718e133a8f
Akash2811000 <akashgupta7383@gmail.com>
2022-07-13 18:35:37 +0530

    feat(booking_post_api) : introduce booking post request

--- Backend/src/model/users.ts
+++ Backend/src/model/users.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    _id: {type: Number,required: true ,unique:true},
+    _id: {type: String,required: true ,unique:true},
     user_name: {type : String},
     user_email :{type : String},
     user_phone_number : {type : String ,minlength : 10 , maxlength : 10},
