185fd4311f5f9f1f4b09c3fc43476317b4f522f6
Akash2811000 <akashgupta7383@gmail.com>
2022-07-13 18:35:37 +0530

    feat(booking_post_api) : introduce booking post request

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -5,12 +5,13 @@ const connection = mongoose.connect('mongodb://localhost:27017/mmtdata');
 import { router as hotelroute} from  './controller/hotel_controller';
 import { router as tourroute} from  './controller/tour_controller';
 import { router as cityroute } from './controller/city_controller';
+import { router as bookingroute } from './controller/booking_controller';
 import { verifyToken,checkRequest } from "./authentication/verify_token";
 import * as admin from 'firebase-admin';
 import credential from "./travelproject22-6b9d4-firebase-adminsdk-2wiay-c9c1876710.json";
-
 import * as dotenv from 'dotenv';
 import { Roomrouter } from './controller/room_controller';
+
 dotenv.config();
 const port = process.env.PORT;
 app.use(express.json());
@@ -25,8 +26,9 @@ app.use(verifyToken,checkRequest);
 
 app.use('/hotel',hotelroute);
 app.use('/room' ,Roomrouter);
-app.use('/tour',tourroute)
+app.use('/tour',tourroute);
 app.use('/city',cityroute);
+app.use('/booking',bookingroute);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('MMT Backend development');
