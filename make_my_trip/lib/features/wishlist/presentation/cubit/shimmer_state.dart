2ec06c40765d85a0488e97e986ddf7c48ddfa04a
JAY-THAKKAR8 <jaythakkar2724@gmail.com>
2022-07-19 15:46:24 +0530

    feat(wishlist_page): added wishlist page design

---
+++ make_my_trip/lib/features/wishlist/presentation/cubit/shimmer_state.dart
@@ -0,0 +1,14 @@
+part of 'shimmer_cubit.dart';
+
+@immutable
+abstract class ShimmerState {}
+
+class ShimmerInitial extends ShimmerState {}
+
+class ShimmerLoading extends ShimmerState{
+
+}
+
+class ShimmerLoaded extends ShimmerState{
+
+}
