dd18c4513a69ab9d48557e8dac8b3055651d58f0
Akash2811000 <akashgupta7383@gmail.com>
2022-07-19 19:12:01 +0530

    feat(hotel_list_page): format code

--- make_my_trip/lib/features/hotel_listing/presentation/cubits/hotel_list_cubit.dart
+++ make_my_trip/lib/features/hotel_listing/presentation/cubits/hotel_list_cubit.dart
@@ -3,18 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:make_my_trip/features/hotel_listing/domain/use_cases/hotel_list_usecase.dart';
 import 'package:make_my_trip/features/hotel_listing/presentation/cubits/hotel_list_state.dart';
 
-
-
-
 class HotelListCubit extends Cubit<HotelListState> {
-  HotelListCubit({required this.hotelListUsecase})
-      : super(HotelListInitial());
+  HotelListCubit({required this.hotelListUsecase}) : super(HotelListInitial());
   final HotelListUsecase hotelListUsecase;
-
 
   getHotelListApi(String hotelName) async {
     var hotelListData = await hotelListUsecase.call(Params(hotelName));
-    hotelListData.fold((l) => {debugPrint(l.toString())}, (r) => emit(GetData(GetList: r)));
-
+    hotelListData.fold(
+        (l) => {debugPrint(l.toString())}, (r) => emit(GetData(GetList: r)));
   }
 }
