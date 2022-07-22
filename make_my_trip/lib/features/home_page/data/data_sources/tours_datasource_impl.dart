526e1b038e392c50ec1ebae6192339251d5c9edc
Akash2811000 <akashgupta7383@gmail.com>
2022-07-22 15:01:26 +0530

    adding header in hotel_detail_remote_data_source.dart hotel_list_datasource_impl.dart images_datasource_impl.dart review_remote_data_source_impl.dart tours_datasource_impl.dart

--- make_my_trip/lib/features/home_page/data/data_sources/tours_datasource_impl.dart
+++ make_my_trip/lib/features/home_page/data/data_sources/tours_datasource_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:make_my_trip/features/home_page/data/data_sources/tours_datasource.dart';
 import '../../../../core/failures/failures.dart';
 import '../../../../utils/constants/base_constants.dart';
@@ -9,11 +10,15 @@ class ToursDataSourceImpl implements ToursDataSource {
   final Dio dio;
 
   ToursDataSourceImpl(this.dio);
+  Future<Options> createDioOptions() async {
+    final userToken = await FirebaseAuth.instance.currentUser!.getIdToken();
+    return Options(headers: {'token': userToken});
+  }
 
   @override
   Future<Either<Failures, List<ToursModel>>> getToursData() async {
     try {
-      final response = await dio.get('${BaseConstant.baseUrl}tour/5');
+      final response = await dio.get('${BaseConstant.baseUrl}tour/5',options: await createDioOptions());
       var result = response.data;
       if (response.statusCode == 200) {
         List<ToursModel> postList = [];
--- make_my_trip/lib/features/hotel_detail/data/data_sources/hotel_detail_remote_data_source.dart
+++ make_my_trip/lib/features/hotel_detail/data/data_sources/hotel_detail_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:make_my_trip/core/failures/failures.dart';
 import 'package:make_my_trip/features/hotel_detail/data/model/hotel_detail_model.dart';
 import 'package:make_my_trip/utils/constants/base_constants.dart';
@@ -11,7 +12,16 @@ abstract class HotelDetailRemoteDataSource {
 class HotelDetailRemoteDataSourceImpl implements HotelDetailRemoteDataSource {
   final Dio dio;
   HotelDetailRemoteDataSourceImpl(this.dio);
+  void printWrapped(String text) {
+    final pattern = new RegExp('.{1,800}'); // 800 is the size of each chunk
+    pattern.allMatches(text).forEach((match) => print(match.group(0)));
+  }
 
+  Future<Options> createDioOptions() async {
+    final userToken = await FirebaseAuth.instance.currentUser!.getIdToken();
+    printWrapped(userToken);
+    return Options(headers: {'token': userToken});
+  }
   @override
   Future<Either<Failures, HotelDetailModel>> getAllHotelDetailData(
       int index) async {
@@ -23,7 +33,7 @@ class HotelDetailRemoteDataSourceImpl implements HotelDetailRemoteDataSource {
       String url) async {
     try {
       print(url);
-      final response = await dio.get(url);
+      final response = await dio.get(url,options: await createDioOptions());
 
       if (response.statusCode == 200) {
         HotelDetailModel hotelDetailModel;
