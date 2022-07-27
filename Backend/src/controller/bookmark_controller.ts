0d45bf4481a41deee2e05ba81d22f1cae0b6d3a1
Akash Gupta <akashgupta7383@gmail.com>
2022-07-27 23:03:36 +0530

    feat(fix_backend_api) : modify some api code

--- Backend/src/controller/bookmark_controller.ts
+++ Backend/src/controller/bookmark_controller.ts
@@ -3,23 +3,23 @@ import express, { Express, Request, Response } from 'express';
 var router = express.Router();
 
 class BookmarkController {
-    static async postBookmark(req:Request,res:Response) {
+    static async postBookmark(req: Request, res: Response) {
         const bookmarkdomain = new BookmarkDomain();
-        await bookmarkdomain.postBookmark(req,res);
+        await bookmarkdomain.postBookmark(req, res);
     }
 
-    static async deleteBookmark(req:Request, res : Response){
+    static async deleteBookmark(req: Request, res: Response) {
         const bookmarkdomain = new BookmarkDomain();
-        await bookmarkdomain.deleteBookmark(req,res);
+        await bookmarkdomain.deleteBookmark(req, res);
     }
-    static async userBookmark(req:Request, res : Response){
+    static async userBookmark(req: Request, res: Response) {
         const bookmarkdomain = new BookmarkDomain();
-        await bookmarkdomain.userBookmark(req,res);
+        await bookmarkdomain.userBookmark(req, res);
     }
 }
 
 
 router.post('/post/:hotel_id', BookmarkController.postBookmark);
-router.delete('/delete/:hotel_id',BookmarkController.deleteBookmark);
-router.get('/user/wishlist',BookmarkController.userBookmark);
-export{router};
+router.delete('/delete/:hotel_id', BookmarkController.deleteBookmark);
+router.get('/user/wishlist', BookmarkController.userBookmark);
+export { router };
--- Backend/src/controller/room_controller.ts
+++ Backend/src/controller/room_controller.ts
@@ -4,43 +4,15 @@ var router: Router = express.Router();
 
 class RoomController {
 
-    // Get Room with Deluxe Type 
-    static async getRoomWithDeluxeType(req: Request, res: Response) {
-        const roomDomain: RoomDomain = new RoomDomain();
-        await roomDomain.getRoomWithDeluxeType(req, res);
-    }
-
-    // Get Room with Semi-Deluxe Type 
-    static async getRoomWithSemiDeluxeType(req: Request, res: Response) {
-        const roomDomain: RoomDomain = new RoomDomain();
-        await roomDomain.getRoomWithSemiDeluxeType(req, res);
-    }
-
-    // Get Room with Super-Deluxe Type 
-    static async getRoomWithSuperDeluxeType(req: Request, res: Response) {
-        const roomDomain: RoomDomain = new RoomDomain();
-        await roomDomain.getRoomWithSuperDeluxeType(req, res);
-    }
-
-    // Get All Available Room
-    static async getAllAvailableRoom(req: Request, res: Response) {
-        const roomDomain = new RoomDomain();
-        await roomDomain.getAllAvailableRoom(req, res);
-    }
-
     //get particular room
-    static async getPerticularRoom(req: Request , res : Response){
+    static async getPerticularRoom(req: Request, res: Response) {
         const roomDomain = new RoomDomain();
-        await roomDomain.getPerticularRoom(req,res);
+        await roomDomain.getPerticularRoom(req, res);
     }
 }
 
 // GET
-router.get("/:id", RoomController.getAllAvailableRoom);
-router.get("/:id/deluxe", RoomController.getRoomWithDeluxeType);
-router.get("/:id/semi-deluxe", RoomController.getRoomWithSemiDeluxeType);
-router.get("/:id/super-deluxe", RoomController.getRoomWithSuperDeluxeType);
-router.get("/getroom/:hotelid/:roomid" , RoomController.getPerticularRoom);
+router.get("/getroom/:hotelid/:roomid", RoomController.getPerticularRoom);
 
 //EXPORT
 export { router };
