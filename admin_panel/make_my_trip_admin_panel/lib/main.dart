8f9e92570e1ccc96de1c569e9da0e558d2d8beb6
arpit.agrawal <arpit.180180107001@gmail.com>
2022-08-16 19:33:40 +0530

    feat(admin_login): login Ui done

--- admin_panel/make_my_trip_admin_panel/lib/main.dart
+++ admin_panel/make_my_trip_admin_panel/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:make_my_trip_admin_panel/features/review_moderation/presentation/pages/review_moderation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:make_my_trip_admin_panel/features/admin_login/presentation/cubit/admin_login_cubit.dart';
+import 'package:make_my_trip_admin_panel/features/admin_login/presentation/pages/admin_login_page.dart';
 import 'package:make_my_trip_admin_panel/utils/constants/string_constants.dart';
 import 'core/theme/make_my_trip_theme.dart';
 import 'core/navigation/app_router.dart' as app_routes;
@@ -22,7 +24,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: StringConstants.appTitle,
       theme: MakeMyTripLightTheme.lightTheme,
-      home: const ReviewModeration(),
+      home: BlocProvider<AdminLoginCubit>(
+        create: (context) => AdminLoginCubit(),
+        child: AdminLoginPage(),
+      ),
     );
   }
 }
